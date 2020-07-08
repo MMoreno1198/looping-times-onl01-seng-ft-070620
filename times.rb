7b916f26f40ee22391f341a9c340980897e48e36
Marcus Moreno <legendary.1198@gmail.com>
2020-07-08 02:38:15 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,11 +1,9 @@
 def using_times
   using_times == "Wingardium Leviosa"
-  puts "#{using_times}
+  puts "#{using_times}"
 end
 7.times do
   puts "#{using_times}"
-  if counter >= 7
-    break
   
 
 end
